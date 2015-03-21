b70d7748b4ffa50b881a23d424ec70c716c2d7df
Jack Yiu <jackinyiu@gmail.com>
2015-03-21 13:54:53 -0400

    Optimized Wheel & added logo

--- code.js
+++ code.js
@@ -1,38 +1,45 @@
 (function () {
     "use strict";
 
-    jQuery.fn.rotate = function(degrees) {
-        $(this).css({'-webkit-transform' : 'rotate(' + degrees + 'deg)',
-                     '-moz-transform' : 'rotate(' + degrees + 'deg)',
-                     '-ms-transform' : 'rotate(' + degrees + 'deg)',
-                     'transform' : 'rotate(' + degrees + 'deg)'});
-        return (this);
-    };
+    var curAngle = 0;
 
     jQuery.fn.animateRotate = function(angle, duration, easing, complete) {
         return this.each(function() {
             var $elem = $(this);
-
-            $({deg: 0}).animate({deg: angle}, {
+            console.log("Target angle:" +(curAngle + angle)%45);
+            var targetAngle = angle + curAngle;
+            if ((curAngle + angle) % 45 == 0) {
+                targetAngle += 22.5;
+            }
+            $elem.animate({deg: targetAngle}, {
                 duration: duration,
                 easing: easing,
                 step: function(now) {
+                    //console.log("Now:" + now);
                     $elem.css({
                         transform: 'rotate(' + now + 'deg)'
                     });
+                    curAngle = now;
+                    if (curAngle === targetAngle) {
+                        $('.rotate').bind("click", rotateClickHandler); // Rebind
+                    }
                 },
                 complete: complete || $.noop
             });
         });
     };
 
-    $('.rotate').click(function() {
+    $('.rotate').keypress(function(e) {
+        $('.rotate').click();
+    });
+
+    $('.rotate').click(rotateClickHandler);
+
+    function rotateClickHandler() {
+        $('.rotate').unbind("click");
+
         var num = Math.floor((Math.random() * 8) + 1);
-        //$(this).rotate(num*45);
         $(this).animateRotate(1440+(num*45)+22.5, 2000, "swing");
-        //$(this).animateRotate((num*45)+22.5);
-        console.log(num*45);
-        console.log((1440+(num*45)+22.5)%360);
-    })
+    }
 
 })();
